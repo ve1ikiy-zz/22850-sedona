621f25aa9c62c7ed16f6a38e5cd2964f90718f5c
Vladimir <ve1ikiy@ya.ru>
2016-05-16 18:59:36 +0300

    На проверку
    Для облегчения проверки:
    - нет тени;
    - на странице каталога элементы формы не совпадают с макетом;
    - на странице каталога нет линий под элементами гостинниц;
    - на главной странице в блоке интро маска не совпадает с макетом;
    - на главной странице в верхнем блоке есть margin-bottom: 4px; чтобы
    нижние элементы совпадали с макетом;
    
    все вышеперечисленные проблемы допущены не специально, а по незнанию как
    их решить :(

--- js/script.js
+++ js/script.js
@@ -1,51 +1,8 @@
-<script>
-  var bookingform = document.querySelector(".login");
-
-  var bookingpopup = document.querySelector(".modal-content");
-  var bookingclose = popup.querySelector(".modal-content-close");
-
-  var form = popup.querySelector("form");
-  var login = popup.querySelector("[name=login]");
-  var password = popup.querySelector("[name=password]");
-
-  var storage = localStorage.getItem("login");
-
-  link.addEventListener("click", function(event) {
-    event.preventDefault();
-    popup.classList.add("modal-content-show");
-
-    if (storage) {
-      login.value = storage;
-      password.focus();
-    } else {
-      login.focus();
-    }
-
-  });
-
-  close.addEventListener("click", function(event) {
-    event.preventDefault();
-    popup.classList.remove("modal-content-show");
-    popup.classList.remove("modal-error");
-  });
-
-  form.addEventListener("submit", function(event) {
-    if (!login.value || !password.value) {
-      event.preventDefault();
-      popup.classList.remove("modal-error");
-      popup.offsetWidth = popup.offsetWidth;
-      popup.classList.add("modal-error");
-    } else {
-      localStorage.setItem("login", login.value);
-    }
-  });
-
-  window.addEventListener("keydown", function(event) {
-    if (event.keyCode === 27) {
-      if (popup.classList.contains("modal-content-show")) {
-        popup.classList.remove("modal-content-show");
-        popup.classList.remove("modal-error");
-      }
-    }
-  });
-</script>
+  var toggle = function()
+  {
+    var mydiv = document.querySelector(".booking-box");
+    if (mydiv.style.display === 'block' || mydiv.style.display === '')
+      mydiv.style.display = 'none';
+      else
+        mydiv.style.display = 'block'
+  }
